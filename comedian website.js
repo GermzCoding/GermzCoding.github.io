82fe938e7ea9d367bb407f3d1813c21855f11961
GermzCoding <49746852+GermzCoding@users.noreply.github.com>
2024-05-02 08:46:31 -0700

    Update comedian website.js

--- comedian website.js
+++ comedian website.js
@@ -15,7 +15,7 @@ function createButton(text, link) {
 
 // Append buttons to the navigation bar
 var navBar = document.getElementById("navBar");
-navBar.querySelector("ul").appendChild(createButton("Home", "#bio"));
+
 navBar.querySelector("ul").appendChild(createButton("Bio", "#bio"));
 navBar.querySelector("ul").appendChild(createButton("Donate", "https://www.paypal.com/donate/?business=LZ23PENCUYC7C&no_recurring=0&currency_code=USD"));
 navBar.querySelector("ul").appendChild(createButton("Book me!", "mailto:mrinsanemethod@gmail.com"));
